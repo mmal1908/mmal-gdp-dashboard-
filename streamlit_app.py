08715c4a41e557f1586b2664c71f3881833efdbe
mmal1908 <mariana@csimedellin.com>
2026-02-26 10:24:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,14 @@ st.image(image, caption='Interfaces Multimodales')
 texto = st.text_input('Escribe algo', 'este es el textooo')
 
 st.write('El texto escrito es:', texto)
+
+st.subheader("Ahora usemos dos columnas:")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+  resp = st.checkbox("Estoy de acuerdo")
+  if resp:
+    st.write('¡Correcto!')
